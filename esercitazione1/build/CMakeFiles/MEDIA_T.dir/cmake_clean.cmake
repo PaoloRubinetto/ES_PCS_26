7add3e51400cbed3974efb39fea76f7f9f8c56d7
PaoloRubinetto <s325663@studenti.polito.it>
2026-03-16 10:16:57 +0000

    Consegna Esercitazione 1

---
+++ esercitazione1/build/CMakeFiles/MEDIA_T.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MEDIA_T.dir/link.d"
+  "CMakeFiles/MEDIA_T.dir/MEDIA_T.cpp.o"
+  "CMakeFiles/MEDIA_T.dir/MEDIA_T.cpp.o.d"
+  "MEDIA_T"
+  "MEDIA_T.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MEDIA_T.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
